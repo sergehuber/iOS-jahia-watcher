7a62102cb5c67fcaf96dc3e839e850d689e1f8f2
Serge Huber <shuber@jahia.com>
2015-03-09 08:09:08 +0100

    Initial Commit

---
+++ WatchTasks/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WatchTasks
+//
+//  Created by Serge Huber on 09.03.15.
+//  Copyright (c) 2015 Jahia Solutions. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WatchTasks/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WatchTasks
+//
+//  Created by Serge Huber on 09.03.15.
+//  Copyright (c) 2015 Jahia Solutions. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
